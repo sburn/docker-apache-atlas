cfa4102c0852c064e732b7053410c59f78af8dad
sburn <vadim@altana.ru>
2020-08-18 23:41:04 +0300

    fix start & stop scripts

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo docker run -d \
+docker run -d \
     -p 21000:21000 \
     -v ${PWD}/pre-conf:/opt/apache-atlas-2.1.0/conf \
     --name atlas \
-    apache-atlas:2.1.0 \
+    sburn/apache-atlas:2.1.0 \
     /opt/apache-atlas-2.1.0/bin/atlas_start.py
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker exec -ti atlas /opt/apache-atlas-2.1.0/bin/atlas_stop.py
+docker stop atlas
